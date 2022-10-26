106b8002b2f61d9eb0f3c4146193d1463ace7263
Bober <thedillerthe@gmail.com>
2022-10-27 01:30:40 +0300

    abstract class begin

--- barkengine.cpp
+++ barkengine.cpp
@@ -1,6 +1,6 @@
 #include "barkengine.hpp"
 
-Village::Village(size_t n, size_t m) : n(n), m(m) { allot_cell(cell, n, m); }
+Village::Village(size_t n, size_t m) : n(n), m(m), cell(NULL) { allot_cell(cell, n, m); }
 
 Village::~Village() {
     for (size_t i = 0; i < n; i++) {
@@ -58,14 +58,14 @@ void Village::print_village() {
 }
 
 void Village::room(size_t& rx, size_t& ry, size_t& rx_max, size_t& ry_max) {
-    rx = rand() % (this->get_n() - 3);
-    ry = rand() % (this->get_m() - 3);
+    rx = rand() % (this->get_n() - 3) + 1;
+    ry = rand() % (this->get_m() - 3) + 1;
     rx_max = rand() % 5 + 4;
     ry_max = rand() % 10 + 8;
 
     for (size_t i = rx; i < rx + rx_max; i++) {
         for (size_t j = ry; j < ry + ry_max; j++) {
-            if (i > this->get_n() - 2 || j > this->get_m() - 2 || i < 1 || j < 1)
+            if (i > this->get_n() - 2 || j > this->get_m() - 2)
                 continue;
             this->get_data_elem(i, j) = ' ';
             this->get_barrier_elem(i, j) = 0;
---
+++ person.hpp
@@ -0,0 +1,13 @@
+#ifndef PERSON_HPP
+#define PERSON_HPP
+
+using namespace std;
+
+#include "dependencies.hpp"
+
+class Person {
+  public:
+    virtual void placement() = 0;
+};
+
+#endif // PERSON_HPP
